73c6e6829ce2959f33e1b6b825f31cd8a24624a1
Capt. Kello <captainkello@gmail.com>
2010-11-20 12:24:37 -1000

    some micropost

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101120215659) do
+ActiveRecord::Schema.define(:version => 20101120220853) do
 
   create_table "members", :force => true do |t|
     t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts", :force => true do |t|
+    t.string   "body"
+    t.integer  "member_id_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
